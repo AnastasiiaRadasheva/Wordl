642a105840ded6bf07e93e2d839c9aa411a3a17f
opilane <opilane@HP-CZC2349HTC>
2025-03-17 08:56:25 +0200

    peredelano

--- wordl.py
+++ wordl.py
@@ -1,4 +1,3 @@
-from colorama import Fore, Back, Style
 import random
 
 words = ["Looma", "Ratas", "Käige", "Sõber", "Laine", "Pagar", "Roosa", "Tunne", "Kalur", "Hinge"]
@@ -14,32 +13,29 @@ You have 6 tries!
 
 print(f"The word has {length} letters. Try to guess it!")
 
-attempts = 6  
+attempts = 6
 
 for j in range(6):
     result = ""
-    print(f"Katse {j+1}/6")
+    print(f"Attempt {j + 1}/6")
 
-    while 1:
-        try:
-            user = str(input("Your guess: ")).lower()
+    while True:
+        user = input("Your guess: ").lower()
 
-            if len(user) > length:
-                print("Your word is too long! Please enter a word with the correct number of letters.")
-            elif len(user) < length:
-                print("Your word is too short! Please enter a word with the correct number of letters.")
-            elif user.isalpha() == False:
-                print("Please enter only letters!")
-            elif len(user) == length:
-                break
-        except:
-            print("Please enter a valid word consisting only of letters!")
+        if len(user) > length:
+            print("Your word is too long! Please enter a word with the correct number of letters.")
+        elif len(user) < length:
+            print("Your word is too short! Please enter a word with the correct number of letters.")
+        elif not user.isalpha():
+            print("Please enter only letters!")
+        elif len(user) == length:
+            break
 
     for i in range(length):
         if user[i] == randomword[i].lower():
-            result += Fore.GREEN + user[i] + Style.RESET_ALL
+            result += f"[{user[i]}]" 
         elif user[i] in randomword.lower() and user[i] != randomword[i].lower():
-            result += Fore.YELLOW + user[i] + Style.RESET_ALL
+            result += f"({user[i]})"  
         else:
             result += "-"
 
@@ -47,7 +43,7 @@ for j in range(6):
     print(result)
 
     if user == randomword.lower():
-        print(f"Palju õnne, te võitsite! Mõeldud sõna on: {randomword}")
+        print(f"Congratulations, you won! The word was: {randomword}")
         break
 else:
-    print(f"Te kaotasite! Mõeldud sõna on: {randomword}")
+    print(f"You lost! The word was: {randomword}")
