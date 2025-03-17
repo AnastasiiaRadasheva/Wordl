3ad38229eaa5c0842001f035e1e6fe3c8b51c085
opilane <opilane@HP-CZC2349HTC>
2025-03-17 09:39:13 +0200

    new

--- wordl.py
+++ wordl.py
@@ -1,49 +1,58 @@
 import random
 
 words = ["Looma", "Ratas", "Käige", "Sõber", "Laine", "Pagar", "Roosa", "Tunne", "Kalur", "Hinge"]
-randomword = random.choice(words)
-length = len(randomword)
-
 print('''
          RULES!
 You have to guess the word.
 You have 6 tries!
-if you guess the word, you win!
-and if tou see [ ] it means that the letter is in the right place.]
+If you guess the word, you win!
+If you see [ ], it means that the letter is in the right place.
 ''')
-print(f"The word has {length} letters. Try to guess it!")
+while True:
+    randomword = random.choice(words)
+    length = len(randomword)
 
-attempts = 6
-print()
-for j in range(6):
-    result = ""
-    print(f"Attempt {j + 1}/6")
+    print(f"The word has {length} letters. Try to guess it!")
 
-    while True:
-        user = input("Your guess: ").lower()
+    for j in range(6):
+        result = ""
+        print(f"Attempt {j + 1}/6")
 
-        if len(user) > length:
-            print("Your word is too long! Please enter a word with the correct number of letters.")
-        elif len(user) < length:
-            print("Your word is too short! Please enter a word with the correct number of letters.")
-        elif not user.isalpha():
-            print("Please enter only letters!")
-        elif len(user) == length:
+        while True:
+            user = input("Your guess: ").lower()
+
+            if len(user) > length:
+                print("Your word is too long! Please enter a word with the correct number of letters.")
+            elif len(user) < length:
+                print("Your word is too short! Please enter a word with the correct number of letters.")
+            elif not user.isalpha():
+                print("Please enter only letters!")
+            elif len(user) == length:
+                break
+
+        for i in range(length):
+            if user[i] == randomword[i].lower():
+                result += f"[{user[i]}]"  
+            elif user[i] in randomword.lower() and user[i] != randomword[i].lower():
+                result += f"({user[i]})"  
+            else:
+                result += "-"  
+
+        result += "\n"
+        print(result)
+
+        if user == randomword.lower():
+            print(f"Congratulations, you won! The word was: {randomword}")
             break
+    else:
+        print(f"You lost! The word was: {randomword}")
 
-    for i in range(length):
-        if user[i] == randomword[i].lower():
-            result += f"[{user[i]}]" 
-        elif user[i] in randomword.lower() and user[i] != randomword[i].lower():
-            result += f"({user[i]})"  
+    answer1 = input("Do you want to continue? (yes/no) ").lower()
+    while True:
+        if answer1 == "yes":
+            break  
+        elif answer1 == "no":
+            print("Thanks for playing!")
+            break
         else:
-            result += "-"
-
-    result += "\n"
-    print(result)
-
-    if user == randomword.lower():
-        print(f"Congratulations, you won! The word was: {randomword}")
-        break
-else:
-    print(f"You lost! The word was: {randomword}")
+            print("Please enter 'yes' or 'no'!")
